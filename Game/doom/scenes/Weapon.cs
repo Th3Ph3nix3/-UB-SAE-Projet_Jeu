56b997cb0dda37a3b77ca94f20ae0487a30e344b
Cozhmisus <teoltz@live.fr>
2025-05-22 15:27:58 +0200

    Added the sprite for the pickup items.

--- Game/doom/scenes/Weapon.cs
+++ Game/doom/scenes/Weapon.cs
@@ -2,29 +2,29 @@ using Godot;
 using System;
 using System.Xml.Resolvers;
 
-
+[GlobalClass]
 public abstract partial class Weapon : Resource
 {
-    #region attributes
-    [Export]
-    public string title; // name of the weapon
+	#region attributes
+	[Export]
+	public string title; // name of the weapon
 
-    [Export]
-    public Texture2D texture; // icon of the weapon
+	[Export]
+	public Texture2D texture; // icon of the weapon
 
 
-    // properties of the projectile / ! \ to change those values, double click on .tres file on the inspector and directly change in Godot
-    [Export]
-    public float damage; // damage of the projectile
-    [Export]
-    public float cooldown; // cooldown until next projectile
-    [Export]
-    public float speed; // speed of the projectile
+	// properties of the projectile / ! \ to change those values, double click on .tres file on the inspector and directly change in Godot
+	[Export]
+	public float damage; // damage of the projectile
+	[Export]
+	public float cooldown; // cooldown until next projectile
+	[Export]
+	public float speed; // speed of the projectile
 
-    [Export]
-    public PackedScene projectile_node = GD.Load<PackedScene>("res://scenes/projectile.tscn"); // scene of the projectile
-    #endregion
-    #region methods
-    public abstract void Activate(PlayerControl _source, Enemy _target, SceneTree _scene_tree); // abstract method, overriden in SingleShot.cs
-    #endregion
+	[Export]
+	public PackedScene projectile_node = GD.Load<PackedScene>("res://scenes/projectile.tscn"); // scene of the projectile
+	#endregion
+	#region methods
+	public abstract void Activate(PlayerControl _source, Enemy _target, SceneTree _scene_tree); // abstract method, overriden in SingleShot.cs
+	#endregion
 }

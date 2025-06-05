de16631a12583fd094183883f421293fba71d8a4
Th3Ph3nix3 <newhugelite@gmail.com>
2025-06-05 20:11:31 +0200

    Death sound implementation

--- Game/doom/Game/Code/GameOverManager.cs
+++ Game/doom/Game/Code/GameOverManager.cs
@@ -1,68 +1,135 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Manages the game over state and related functionalities.
+/// </summary>
 public partial class GameOverManager : Node
 {
-	// Singleton instance for global access.
+    // #region Singleton Instance
+
+    /// <summary>
+    /// Singleton instance for global access to the GameOverManager.
+    /// </summary>
     public static GameOverManager Instance { get; private set; }
-	
-	// Current settings menu state
-	private bool inGameOver = false;
-	private AudioStreamPlayer clickcliksound;
-	// Current pause state of the game.
-	private static bool isPaused = false;
 
-	#region Signals
+    // #endregion
 
-	/// <summary>
-	/// Signal emitted when settings menu visibility changes
-	/// </summary>
-	/// <param name="inGameOver">True if settings menu is visible</param>
-	[Signal]
+    // #region Private Fields
+
+    /// <summary>
+    /// Current game over menu state.
+    /// </summary>
+    private bool inGameOver = false;
+
+    /// <summary>
+    /// Audio player for click sounds.
+    /// </summary>
+    private AudioStreamPlayer clickcliksound;
+
+    /// <summary>
+    /// Audio player for background music.
+    /// </summary>
+    private AudioStreamPlayer musicPlayer;
+
+    /// <summary>
+    /// Audio player for death sound.
+    /// </summary>
+    private AudioStreamPlayer deathSound;
+
+    /// <summary>
+    /// Position in seconds to loop the death sound.
+    /// </summary>
+    private float loopStartPosition = 7.0f;
+
+    /// <summary>
+    /// Current pause state of the game.
+    /// </summary>
+    private static bool isPaused = false;
+
+    // #endregion
+
+    // #region Signals
+
+    /// <summary>
+    /// Signal emitted when game over menu visibility changes.
+    /// </summary>
+    /// <param name="inGameOver">True if game over menu is visible.</param>
+    [Signal]
     public delegate void GameOverToggleEventHandler(bool inGameOver);
 
-	#endregion
+    // #endregion
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Instance = this;
-	}
+    // #region Godot Methods
 
-	public static void GameOver()
-	{
-		isPaused = !isPaused;
-		Instance.EmitSignal(SignalName.GameOverToggle, isPaused);
-		Instance.GetTree().Paused = isPaused;
-		Instance.ToggleinGameOver();
-	}
-	
-	public void _on_start_pressed()
-	{
-		clickcliksound = GetNode<AudioStreamPlayer>("ClickClickSound");
-		clickcliksound.Play();
-		isPaused = !isPaused;
-		EmitSignal(SignalName.GameOverToggle, isPaused);
-		GetTree().Paused = isPaused;
-		GetTree().ChangeSceneToFile("res://Game/Scenes/Game_Test.tscn");
-	}
-	
-	/// <summary>
-	/// Handles quit button press to close settings menu
-	/// </summary>
-	public void _on_quit_button_pressed()
-	{
-		clickcliksound = GetNode<AudioStreamPlayer>("ClickClickSound");
-		clickcliksound.Play();
-		GetTree().Quit();
-	}
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// Initializes the GameOverManager instance and audio players.
+    /// </summary>
+    public override void _Ready()
+    {
+        Instance = this;
+        musicPlayer = GetNode<AudioStreamPlayer>("Music");
+        deathSound = Instance.GetNode<AudioStreamPlayer>("Death");
+    }
 
-	/// <summary>
-	/// Toggles settings menu visibility and emits state change signal
-	/// </summary>
-	public void ToggleinGameOver()
-	{
-		inGameOver = !inGameOver;
-		EmitSignal(SignalName.GameOverToggle, inGameOver);
-	}
+    // #endregion
+
+    // #region Public Methods
+
+    /// <summary>
+    /// Handles the game over state, toggles pause, emits signals, and plays death sound.
+    /// </summary>
+    public static void GameOver()
+    {
+        isPaused = !isPaused;
+        Instance.EmitSignal(SignalName.GameOverToggle, isPaused);
+        Instance.GetTree().Paused = isPaused;
+        Instance.ToggleinGameOver();
+        Instance.musicPlayer.Stop();
+        Instance.deathSound.Finished += Instance.AudioFinished;
+        Instance.deathSound.Play();
+    }
+
+    /// <summary>
+    /// Called when the death sound finishes playing. Loops the death sound from the specified position.
+    /// </summary>
+    private void AudioFinished()
+    {
+        deathSound.Play(loopStartPosition);
+    }
+
+    /// <summary>
+    /// Handles the start button press event. Plays a sound and changes the scene to the game scene.
+    /// </summary>
+    public void _on_start_pressed()
+    {
+        clickcliksound = GetNode<AudioStreamPlayer>("ClickClickSound");
+        clickcliksound.Play();
+        isPaused = !isPaused;
+        EmitSignal(SignalName.GameOverToggle, isPaused);
+        GetTree().Paused = isPaused;
+        GetTree().ChangeSceneToFile("res://Game/Scenes/Game_Test.tscn");
+    }
+
+    /// <summary>
+    /// Handles the quit button press event. Plays a sound and quits the game.
+    /// </summary>
+    public void _on_quit_button_pressed()
+    {
+        clickcliksound = GetNode<AudioStreamPlayer>("ClickClickSound");
+        clickcliksound.Play();
+        GetTree().Quit();
+    }
+
+    /// <summary>
+    /// Toggles the game over menu visibility and emits the state change signal.
+    /// </summary>
+    public void ToggleinGameOver()
+    {
+        inGameOver = !inGameOver;
+        EmitSignal(SignalName.GameOverToggle, inGameOver);
+    }
+
+    // #endregion
 }
--- Game/doom/Game/Code/ToggleVisibilityGameOver.cs
+++ Game/doom/Game/Code/ToggleVisibilityGameOver.cs
@@ -1,31 +1,47 @@
 using Godot;
 using System;
 
+/// <summary>
+/// Manages the visibility of UI elements based on the game over state.
+/// </summary>
 public partial class ToggleVisibilityGameOver : CanvasLayer
 {
-	/// <summary>
-	/// Determines if this node should be visible when in Game Over.
-	/// Set in the inspector.
-	/// </summary>
-	[Export]
-	bool visibleGameOver = true;
+    // #region Exported Variables
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		// Subscribe to the settings menu visibility toggle signal
-		GameOverManager.Instance.GameOverToggle += ToggleVisibility;
+    /// <summary>
+    /// Determines if this node should be visible when in Game Over.
+    /// Set in the inspector.
+    /// </summary>
+    [Export]
+    bool visibleGameOver = true;
 
-		// Hide the node initially if it should only be shown in Game Over
+    // #endregion
+
+    // #region Godot Methods
+
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// Subscribes to the game over toggle signal and initializes visibility.
+    /// </summary>
+    public override void _Ready()
+    {
+        // Subscribe to the game over visibility toggle signal
+        GameOverManager.Instance.GameOverToggle += ToggleVisibility;
+
+        // Hide the node initially if it should only be shown in Game Over
         if (!visibleGameOver) return;
 
-		Hide();
-	}
-	
-	/// <summary>
-    /// Callback to toggle this node's visibility based on Game Over.
+        Hide();
+    }
+
+    // #endregion
+
+    // #region Private Methods
+
+    /// <summary>
+    /// Callback to toggle this node's visibility based on the game over state.
     /// </summary>
-    /// <param name="inGameOver">True if the settings menu is open.</param>
+    /// <param name="inGameOver">True if the game over menu is open.</param>
     private void ToggleVisibility(bool inGameOver)
     {
         if (visibleGameOver == inGameOver)
@@ -37,4 +53,6 @@ public partial class ToggleVisibilityGameOver : CanvasLayer
             Hide();
         }
     }
+
+    // #endregion
 }
